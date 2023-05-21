f12fae7230a16947c657b647c780ad1c666ccbe6
Trinhnt3027 <trinhnt3027@gmail.com>
2023-05-21 18:33:14 +0700

    Update logic for files

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=trinhnt30/ml-api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login -u trinhnt30
+docker tag ml-api $dockerpath:latest
+
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
